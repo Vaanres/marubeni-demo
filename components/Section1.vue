53f020d902cbc1fbe20e1cfcfc0225503f89cea0
Vaanres <vaanres@gmail.com>
2019-12-29 18:27:22 +0700

    update

--- components/Section1.vue
+++ components/Section1.vue
@@ -1,5 +1,13 @@
 <template>
-  <p>Section 1</p>
+  <section
+    class="section section-1 d-flex justify-content-center align-items-center"
+  >
+    <div class="container">
+      <h2 class="mb-0">Fairness</h2>
+      <h2 class="mb-0">Innovation</h2>
+      <h2 class="mb-0">Harmony</h2>
+    </div>
+  </section>
 </template>
 
 <script>
--- components/Section2.vue
+++ components/Section2.vue
@@ -1,5 +1,15 @@
 <template>
-  <p>Section 2</p>
+  <section
+    class="section section-1 d-flex justify-content-center align-items-center"
+  >
+    <div class="container">
+      <div class="section-header">
+        <h6 class="sub-title mb-4">Our business</h6>
+        <h2 class="title mb-0">Sustainable</h2>
+        <h2 class="title mb-0">Growth in Asia</h2>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
