6c3aa0ed854598c39ccd0096714f3b85b33e67af
Vaanres <vaanres@gmail.com>
2019-12-29 18:40:01 +0700

    update

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,9 @@
 <template>
-  <p>Header</p>
+  <header class="header">
+    <nav class="navbar container fixed-top navbar-light py-5">
+      <img class="header-logo mr-1" src="logo.svg" alt="logo" />
+    </nav>
+  </header>
 </template>
 
 <script>
--- components/Section3.vue
+++ components/Section3.vue
@@ -1,5 +1,19 @@
 <template>
-  <p>Section 3</p>
+  <section
+    class="section section-1 d-flex justify-content-center align-items-center"
+  >
+    <div class="container">
+      <div class="section-header">
+        <h6 class="sub-title mb-4">Our vision</h6>
+        <h2 class="title mb-0">Bring happiness</h2>
+        <h2 class="title mb-0">to people</h2>
+      </div>
+
+      <div class="section-header">
+        <h2 class="title mb-0">& our environment</h2>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
